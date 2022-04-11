4e8a3644b1669e596ff427ae38403a827ad8d741
Ife-puti <ifeoluwaadeputi90@gmail.com>
2022-04-10 21:06:13 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,21 +8,20 @@
  */
 int main(void)
 {
-	int d;
+	int d1, d2;
 
-	for (d = 0; d < 90; d++)
+	for (d1 = 0; d1 < 9; d1++)
 	{
-		for ()
+		for (d2n = d1 + 1; d2 < 10; d2++)
 		{
-			putchar((d / 10) + '0');
-			putchar((d % 10) + '0');
+			putchar((d1 % 10) + '0');
+			putchar((d2 % 10) + '0');
 		}
-		if (d != 89)
+		if (d1 == 8 && d2 ==9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
-	putchar('\n');
 	return (0);
 }
