386e5de85d9d09e1655ec8877fae02b57c94d7a2
Ife-puti <ifeoluwaadeputi90@gmail.com>
2022-04-25 22:13:51 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,11 +9,10 @@
 
 char *_strcat(char *dest, char *src)
 {
-       int i = 0, j = 0;
-
-       while (dest[i] != '\0' )
+	int i = 0, j = 0;
+       
+	while (dest[i] != '\0' )
 		i++;
-  
 
 	while (src[j] != '\0')
 	{
@@ -21,7 +20,7 @@ char *_strcat(char *dest, char *src)
         	i++;
 		j++;
 	}
-
+	
 	dest[i] = '\0';
 
 	return (dest);
