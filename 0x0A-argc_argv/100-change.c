8528f47a8bf8b40f8f7b58f4d6f3b3637b1b0f0e
Ife-puti <ifeoluwaadeputi90@gmail.com>
2022-05-05 00:12:35 -0700

    task 100

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the function
+ * @argc: counts the arguments of the function
+ * @argv: gives the value of the arguments passed to the function
+ *
+ * Return: Return always success
+ */
+
+int main(int argc, char *argv[])
+{
+	int cents, coins = 0;
+
+	if (argc == 2)
+	{
+		cents = atoi(*(argv + 1));
+		while (cents > 0)
+		{
+			if (cents % 25 < cents)
+			{
+				cents -= 25;
+				coins++;
+			}
+			else if (cents % 5 < cents)
+			{
+				cents -= 1;
+				coins++;
+			}
+		}
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", coins);
+	return (0);
+}
