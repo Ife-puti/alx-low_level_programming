e32b738b8ce05ca1b2311603d24774c7d8d3dfe1
Ife-puti <ifeoluwaadeputi90@gmail.com>
2022-05-15 20:34:47 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include "function_pointers.h
+#include "function_pointers.h"
 
 /**
  * print_name - prints a name
@@ -8,10 +8,10 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-  if (name == NULL || f == NULL)
-    return;
+	if (name == NULL || f == NULL)
+		return;
 
-  f(name);
+	f(name);
 }
 
 	
